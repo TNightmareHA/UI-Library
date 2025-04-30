ec478af3bbcc3a0cbd176191b40f65fa6fb1f3cf
TNightmareHA <99566192+TNightmareHA@users.noreply.github.com>
2025-04-30 16:19:35 +0800

    Update PlayStation.lua

--- PlayStation.lua
+++ PlayStation.lua
@@ -1,4 +1,4 @@
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/TNightmareHA/Backup/refs/heads/main/PlayStation.lua"))()
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/TNightmareHA/Backup/refs/heads/main/PlayStation"))()
 
 local UI = GUI:CreateWindow("the um","lop")
 
